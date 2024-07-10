35b37b194decce5b5eb645dc61b50ff3a6ddbc05
nateleong <nathanleong95@gmail.com>
2024-07-11 02:33:20 +0800

    ok

--- 1598. Crawler Log Folder.py
+++ 1598. Crawler Log Folder.py
@@ -1,18 +1,19 @@
 def minOperations(logs: list[str]) -> int:
     depth = 0
     for log in logs:
-        if log == "../":
+        if log == "../" and depth > 0:
             depth -= 1
-        elif log == "./":
+        elif log == "./" or log == "../":
             depth == depth
         else:
             depth += 1
     return print(depth)
 
-logs = ["d1/","d2/","./","d3/","../","d31/"]
+logs = ["./","../"]
 
 minOperations(logs)
 
+#https://leetcode.com/problems/crawler-log-folder/submissions/1316767629?envType=daily-question&envId=2024-07-10
 
 #Input: logs = ["d1/","d2/","../","d21/","./"]
 #Output: 2
