da7f1948b95abb42ac46c76146a01fc53976078c
nateleong <nathanleong95@gmail.com>
2024-07-11 14:40:52 +0800

    2108. Find First Palindromic String in the Array

---
+++ 2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,31 @@
+
+def firstPalindrome(words: list[str]) -> str:
+    
+    for word in words:
+        isPalindrome = True
+        num = len(word)
+        i = 0
+        for i in range((num//2) + 1):
+            if word[i] != word[num-i-1]:
+                isPalindrome = False
+
+        if isPalindrome:
+            return word
+
+array1 = ["abc","car","racecar","cool"]
+
+print(firstPalindrome(array1))
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            isPalindrome = 1
+            num = len(word)
+            i = 0
+            for i in range((num//2) + 1):
+                if word[i] != word[num-i-1]:
+                    isPalindrome = 0
+
+            if isPalindrome == 1:
+                return word
+        return ""
