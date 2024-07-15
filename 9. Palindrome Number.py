96a46908dba8ee557472ac52f3f495c90e6cabf4
nateleong <nathanleong95@gmail.com>
2024-07-15 23:06:00 +0800

    added alt method

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -7,3 +7,19 @@ class Solution:
                 return False
         return True
 
+
+
+def isPalindrome(self, x: int) -> bool:
+    #divide by 10 and modulus 10 to get the last number
+
+    if x < 0:
+        return False
+    
+    reverse = 0
+    xcopy = x
+
+    while x > 0:
+        reverse = (reverse * 10) + (x % 10)
+        x = x / 10
+        
+    return xcopy == reverse
