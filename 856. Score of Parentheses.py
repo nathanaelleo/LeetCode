63278ba5a5a8fe682e4fa2e29094a7c619c264ba
nateleong <nathanleong95@gmail.com>
2024-07-15 21:45:06 +0800

    Upload

--- 856. Score of Parentheses.py
+++ 856. Score of Parentheses.py
@@ -7,6 +7,7 @@ def removeOuterParentheses(s: str):
         #if fulfils objective, collect
         collection = []
         collectionv2 = []
+        collectionv3 = []
         #condition, when it is open, collect
         for i in range(len(s)):
             if s[i] == "(":
@@ -19,40 +20,39 @@ def removeOuterParentheses(s: str):
                 for j in range(len(collectionv2)):
                     if collectionv2[j] > collectionv2[j - 1] and collectionv2[j] > collectionv2[j + 1]:
                         collection.append(collectionv2[j])
-                maxval = max(collection)
+                
+                maxvalv1 = max(collection)
+                print(collection)
 
-
-
-                while len(collection) > 1:
-                    for k in collection:
-                        if k == maxval:
-                            score = score + 1
-                            collection.pop()
-
-                print(maxval)        
+                k = 0
+                while max(collection) != min(collection):
+                    maxval = max(collection)
+                    if collection[k] == maxval:
+                        score = score + 1
+                        collection.pop(k)
+                        k = 0 
+                    else:
+                        k = k + 1
+                    if maxvalv1 != maxval:
+                        score = score * 2
                 print(collection)
                 print(score)
-                #    maxval = max(collection)
-                #score = score * 2
-                #collectionv2.clear()
-                #collection.clear()
-        
-                        
-
-                
+                maxval = max(collection)
                 
 
-            #print(indicator)
-            #print(score)
-            #print(s[i])
-            #print(collection)
-            #print(score)
-            #print(s[i])
-        #print(collection)
-         
+                if max(collection) == min(collection):
+                    score = len(collection) * 2 ** (maxval-1) + score
+                collectionv2.clear()
+                collection.clear()
+                collectionv3.append(score)
+                score = 0
+            
+        print(sum(collectionv3))
+
         #print(strings.join(collection))
+removeOuterParentheses("()(())()(((()(()())))(((())(()))()))(((())))")
 #removeOuterParentheses("((()()))")
-removeOuterParentheses("(()(()))")
+#removeOuterParentheses("(()(()))")
 #removeOuterParentheses("()()")
 #removeOuterParentheses("(()())(())(()(()))")
 #4 + 2 + (3) * 2 
