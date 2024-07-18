170ed5a90d5bff05e47edc39ee1d2ebe71d426ae
nateleong <nathanleong95@gmail.com>
2024-07-19 02:34:18 +0800

    kermit

---
+++ 4.Anagram Group (Neetcode).py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = defaultdict(list)
+
+        for i in strs:
+            count = [0] * 26
+            
+            for c in i:
+                count[ord(c) - ord("a")] += 1
+
+            dic[tuple(count)].append(i)
+
+        return dic.values()            
