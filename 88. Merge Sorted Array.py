505b46168e694f34080eff57921ec33edc713d63
nateleong <nathanleong95@gmail.com>
2024-08-01 00:52:13 +0800

    Kermit

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -7,3 +7,18 @@ class Solution:
             nums1[m+i] = nums2[i]
 
         return nums1.sort()
+    
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
